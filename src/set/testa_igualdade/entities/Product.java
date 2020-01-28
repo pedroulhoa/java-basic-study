cc72d53904a08cbdf84d5f76a1e6bd7dee577f18
Pedro Ulhoa <pedroulhoa@live.com>
2020-01-27 21:37:51 -0300

    new topic set

--- src/set/testa_igualdade/entities/Product.java
+++ src/set/testa_igualdade/entities/Product.java
@@ -2,7 +2,7 @@ package set.testa_igualdade.entities;
 
 import java.util.Objects;
 
-public class Product {
+public class Product implements Comparable<Product> {
 
     private String name;
     private Double price;
@@ -40,5 +40,18 @@ public class Product {
     @Override
     public int hashCode() {
         return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return name.toUpperCase().compareTo(other.getName().toUpperCase());
     }
 }
