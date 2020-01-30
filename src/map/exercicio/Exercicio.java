09c693727dce77fa7acb1f1c976a48c3abb677dd
Pedro Ulhoa <pedroulhoa@live.com>
2020-01-29 21:36:15 -0300

    new map exercice

---
+++ src/map/exercicio/Exercicio.java
@@ -0,0 +1,52 @@
+package map.exercicio;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter file full path: ");
+        String path = sc.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            Map<String, Integer> candidates =  new LinkedHashMap<>();
+
+            String line = br.readLine();
+
+            while(line != null) {
+                String[] values = line.split(",");
+
+                if (candidates.containsKey(values[0])) {
+                    Integer increment = candidates.get(values[0]) + Integer.valueOf(values[1]);
+                    candidates.put(values[0], increment);
+                } else {
+                    candidates.put(values[0], Integer.valueOf(values[1]));
+                }
+
+                line = br.readLine();
+            }
+
+            for (String key : candidates.keySet()) {
+                System.out.println(key + ": " + candidates.get(key));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+
+
+        sc.close();
+    }
+
+}
