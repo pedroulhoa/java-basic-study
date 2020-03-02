67a3f311ce0731ca6a2010f2b025e13a9e3a45bc
Pedro Ulhoa <pedroulhoa@live.com>
2020-03-02 20:30:12 -0300

    lambda: predicate

---
+++ src/funcional_lambda/predicate/exemplo/utils/ProductPredicate.java
@@ -0,0 +1,13 @@
+package funcional_lambda.predicate.exemplo.utils;
+
+import funcional_lambda.predicate.exemplo.entities.Product;
+
+import java.util.function.Predicate;
+
+public class ProductPredicate implements Predicate<Product> {
+
+    @Override
+    public boolean test(Product product) {
+        return product.getPrice() >= 100.0;
+    }
+}
