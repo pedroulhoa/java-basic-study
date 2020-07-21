8885b63cbddf69c6a62f3a41b0fcd02f2a470cd8
Pedro Ulhoa <pedroulhoa@live.com>
2020-07-20 22:20:08 -0300

    lambda: funcao como parametro

---
+++ src/funcional_lambda/funcao_como_parametro/exemplo/application/Program.java
@@ -0,0 +1,33 @@
+package funcional_lambda.funcao_como_parametro.exemplo.application;
+
+import funcional_lambda.funcao_como_parametro.exemplo.entities.Product;
+import funcional_lambda.funcao_como_parametro.exemplo.model.services.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        ProductService ps = new ProductService();
+
+        double sum = ps.filteredSum(list, product -> product.getPrice() < 100.0);
+        double sum2 = ps.filteredSum(list, product -> product.getName().charAt(0) == 'T');
+
+        System.out.println("Sum = " + String.format("%.2f", sum));
+        System.out.println("Sum = " + String.format("%.2f", sum2));
+
+    }
+
+}
---
+++ src/funcional_lambda/funcao_como_parametro/exemplo/model/services/ProductService.java
@@ -0,0 +1,20 @@
+package funcional_lambda.funcao_como_parametro.exemplo.model.services;
+
+import funcional_lambda.funcao_como_parametro.exemplo.entities.Product;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+
+    public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+        double sum = 0.0;
+        for (Product p : list) {
+            if (criteria.test(p)) {
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+
+}
