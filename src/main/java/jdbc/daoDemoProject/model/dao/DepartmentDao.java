a7b59b9d4bd3ad80113df8dafac8b4ac53db78f2
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-05 20:11:17 -0300

    feat: create dao interfaces

---
+++ src/main/java/jdbc/daoDemoProject/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package jdbc.daoDemoProject.model.dao;
+
+import jdbc.daoDemoProject.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+}
