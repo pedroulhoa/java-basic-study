526bac8e7ba37b7e867c588eb26dba04ffb19435
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-04 21:45:26 -0300

    feat: seller entity class

--- src/main/java/jdbc/daoDemoProject/application/Program.java
+++ src/main/java/jdbc/daoDemoProject/application/Program.java
@@ -1,6 +1,9 @@
 package jdbc.daoDemoProject.application;
 
 import jdbc.daoDemoProject.model.entities.Department;
+import jdbc.daoDemoProject.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,8 @@ public class Program {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
