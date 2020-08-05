9dcbed75d450821b466043e8f601368160c2eeef
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-05 20:42:13 -0300

    feat: findById impl

--- src/main/java/jdbc/daoDemoProject/application/Program.java
+++ src/main/java/jdbc/daoDemoProject/application/Program.java
@@ -1,5 +1,7 @@
 package jdbc.daoDemoProject.application;
 
+import jdbc.daoDemoProject.model.dao.DaoFactory;
+import jdbc.daoDemoProject.model.dao.SellerDao;
 import jdbc.daoDemoProject.model.entities.Department;
 import jdbc.daoDemoProject.model.entities.Seller;
 
@@ -9,9 +11,9 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/main/java/jdbc/daoDemoProject/model/dao/DaoFactory.java
+++ src/main/java/jdbc/daoDemoProject/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package jdbc.daoDemoProject.model.dao;
 
 import jdbc.daoDemoProject.model.dao.impl.SellerDaoJDBC;
+import jdbc.db.DB;
 
 public class DaoFactory {
 
-    public static  SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
 }
